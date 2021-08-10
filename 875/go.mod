ce7ba6e8df219fb1883a893675353c41aa39060d
kevin <wanjunfeng@gmail.com>
2021-08-10 15:11:10 +0800

    add test code

---
+++ 875/go.mod
@@ -0,0 +1,5 @@
+module issue
+
+go 1.16
+
+require github.com/tal-tech/go-zero v1.1.10
---
+++ 875/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/tal-tech/go-zero/core/conf"
+	"github.com/tal-tech/go-zero/rest"
+	"github.com/tal-tech/go-zero/rest/router"
+)
+
+func main() {
+	var c rest.RestConf
+	conf.MustLoad("config.yaml", &c)
+
+	// init c here
+	r := router.NewRouter()
+	r.SetNotAllowedHandler(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		fmt.Println("not allowed")
+	}))
+	r.SetNotFoundHandler(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		fmt.Println("not found")
+	}))
+	srv := rest.MustNewServer(c, rest.WithRouter(r))
+	srv.AddRoute(rest.Route{
+		Method: http.MethodPost,
+		Path:   "/hello",
+		Handler: func(rw http.ResponseWriter, r *http.Request) {
+		},
+	})
+	defer srv.Stop()
+	srv.Start()
+}
